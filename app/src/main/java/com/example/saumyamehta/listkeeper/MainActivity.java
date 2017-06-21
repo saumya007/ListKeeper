df932988cc355a1a084e8331db20f7d6830ab7da
Saumya Mehta <saumyamehta@saumyas-macbook-air.local>
2017-06-21 16:33:54 +0530

    Added bitmap optimization for images and event handling on button

--- app/src/main/java/com/example/saumyamehta/listkeeper/MainActivity.java
+++ app/src/main/java/com/example/saumyamehta/listkeeper/MainActivity.java
@@ -2,12 +2,29 @@ package com.example.saumyamehta.listkeeper;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
 
 public class MainActivity extends AppCompatActivity {
+    private Toolbar mToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
+        initBackgroundImage();
+    }
+
+    private void initBackgroundImage() {
+        ImageView imgBack = (ImageView) findViewById(R.id.iv_background);
+
+        Glide.with(this)
+                .load(R.drawable.background)
+                .centerCrop()
+                .into(imgBack);
     }
 }
